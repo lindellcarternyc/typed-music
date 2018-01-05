28499fd6f68bb54d96e183c7ce1286bafabfdd0f
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-05 18:48:19 -0500

    Moved to models

---
+++ src/models/index.ts
@@ -0,0 +1,28 @@
+import BaseDuration, { BaseDurationValue } from './baseduration'
+import Duration from './duration'
+
+import Beat from './beat'
+import Frequency from './frequency'
+
+import Meter from './meter'
+import Tempo from './tempo'
+
+import Midinote from './midinote'
+
+import Note from './note'
+import Pitch from './pitch'
+import Pitchclass, { PitchclassValue } from './pitchclass'
+
+import * as PitchnameModel from './pitchnames'
+
+export {
+  BaseDuration, BaseDurationValue,
+  Duration,
+  Beat,
+  Frequency,
+  Meter,
+  Tempo,
+  Midinote,
+  Note, Pitch, Pitchclass, PitchclassValue,
+  PitchnameModel
+}
