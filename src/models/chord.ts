0635064362ae96077ccca90c10877b6f8476ad10
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-05 19:27:24 -0500

    Added static fromString method

--- src/models/chord.ts
+++ src/models/chord.ts
@@ -4,6 +4,23 @@ export default class Chord {
   readonly pitches: Pitch[]
   readonly duration: Duration
 
+  static fromString(input: string): Chord {
+    const firstSplit = input.split(']')
+    // console.dir(firstSplit, null, 2)
+    
+    const durationString = firstSplit[1]
+    const duration = Duration.fromString(durationString)
+
+    const pitchesInput = firstSplit[0].slice(1).split(' ')
+    // console.log(pitchesInput)
+    const pitches = pitchesInput.map(input => {
+      return Pitch.fromString(input)
+    })
+    console.dir(pitches, null, 2)
+
+    return new Chord(duration, ...pitches)
+  }
+
   constructor(duration: Duration, ...pitches: Pitch[]) {
     this.duration = duration
     this.pitches = pitches
