f4364758315918f8bafd9904ab82510d43b78cef
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-05 19:05:01 -0500

    Added constructor

--- src/models/chord.ts
+++ src/models/chord.ts
@@ -1,3 +1,11 @@
+import { Pitch, Duration } from '.'
+
 export default class Chord {
-  
+  readonly pitches: Pitch[]
+  readonly duration: Duration
+
+  constructor(duration: Duration, ...pitches: Pitch[]) {
+    this.duration = duration
+    this.pitches = pitches
+  }
 }
